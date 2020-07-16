a974d22cfbe806bc9618f9ce966c1aa72e59332c
Dustin Mowcomber <dustin.mowcomber@sendgrid.com>
2020-07-16 16:10:16 -0700

    wip

--- neopixel-web-server.ino
+++ neopixel-web-server.ino
@@ -1,14 +1,14 @@
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+// #include <ESP8266WiFi.h> // already gets included through ESP8266WebServer
+// #include <WiFiClient.h>  // already gets included through ESP8266WebServer
 #include <WS2812FX.h>
 
+/* Set these to your desired credentials. */
 #ifndef APSSID
 #define APSSID "test"
 #define APPSK  "testing123"
 #endif
 
-/* Set these to your desired credentials. */
 const char *ssid = APSSID;
 const char *password = APPSK;
 
@@ -18,85 +18,7 @@ const char *password = APPSK;
 ESP8266WebServer server(80);
 WS2812FX ws2812fx(pixelCount, pixelPin, NEO_GRB + NEO_KHZ800);
 
-void handleRoot() {
-  server.send(200, "text/html", R"(
-  <!DOCTYPE html><html><head>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
-    <script type='text/javascript'>
-      console.log("hellow from js");
-      $(document).ready(function(){
-        $("button").on('click', function(){
-          console.log('click!');
-
-          var button = $(this);
-          console.log('1');
-
-          curr = button.attr("value");
-          console.log('2');
-
-          console.log(curr);
-          if (curr == 'color') {
-            $.ajax({
-              url: '/color',
-              success: function(data){},
-              error: function(error){
-                console.log("error: " + JSON.stringify(error));
-              }
-            });
-          } else if (curr == 'rainbow') {
-            $.ajax({
-              url: '/rainbow',
-              success: function(data){},
-              error: function(error){
-                console.log("error: " + JSON.stringify(error));
-              }
-            });
-          }
-        });
-      });
-    </script>
-  </head>
-  <body>
-    <h1>You are connected</h1>
-    <button value='color' id='color'>color</button>
-    <button value='rainbow' id='rainbow'>rainbow</button>
-  </body></html>
-  )");
-}
-
-void handleOrange() {
-  uint32 color = 0xFF4301;
-  ws2812fx.setColor(color);
-  ws2812fx.setMode(FX_MODE_STATIC);
-  ws2812fx.setBrightness(20);
-  ws2812fx.service();
-
-  String colorStr = String(color, HEX);
-  colorStr.toUpperCase();
-
-  String message = "color set to 0x" + colorStr;
-  server.send(200, "text/html", message);
-}
-
-void handleMode() {
-  uint8 mode = FX_MODE_RAINBOW_CYCLE;
-
-  ws2812fx.setSpeed(DEFAULT_SPEED);
-  ws2812fx.setBrightness(5);
-  ws2812fx.setMode(mode);
-  ws2812fx.start();
-  ws2812fx.show();
-  ws2812fx.service();
-
-  String message = "mode set to " + String(mode);
-  // String message = "mode set to ";
-  server.send(200, "text/html", message);
-}
-
 void setup() {
-  delay(1000);
   Serial.begin(115200);
   Serial.println();
 
@@ -124,12 +46,148 @@ void setup() {
   Serial.print("AP IP address: ");
   Serial.println(myIP);
   server.on("/", handleRoot);
-  server.on("/color", handleOrange);
-  server.on("/rainbow", handleMode);
+  server.on("/color", handleColor);
+  server.on("/rainbow", handleRainbow);
+  server.on("/orange", handleOrange);
+  server.on("/brightness", handleBrightness);
   server.begin();
   Serial.println("HTTP server started");
 }
 
 void loop() {
   server.handleClient();
+}
+
+void handleRoot() {
+  server.send(200, "text/html", R"(
+  <!DOCTYPE html><html><head>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
+    <script type='text/javascript'>
+      console.log("hellow from js");
+
+      var callEndpoint = function(path) {
+        $.ajax({
+          url: path,
+          success: function(data){
+            $("#status").text(path + ' done');
+          },
+          error: function(error){
+            $("#status").text('Error: ' + JSON.stringify(error));
+          }
+        });
+      };
+
+      $(document).ready(function(){
+        $("button").on('click', function(){
+          // var button = $(this); // TODO: update the button color to indicate that its active?
+          var buttonID = this.id;
+          if (buttonID == 'color') {
+            var colorHex = $('#colorHex').val();
+            callEndpoint('/color?color=' + colorHex);
+          } else if (buttonID == 'rainbow') {
+            callEndpoint('/rainbow')
+          } else if (buttonID == 'brightness') {
+            var brightnessInput = $('#brightnessInput').val();
+            callEndpoint('/brightness?brightness=' + brightnessInput);
+          } else if (buttonID == 'orange') {
+            callEndpoint('/orange')
+          }
+        });
+      });
+    </script>
+  </head>
+  <body>
+    <h1>You are connected</h1>
+    <button id='color'>color</button>
+    <input type="text" id="colorHex"><br />
+    <button id='brightness'>brightness 0-255</button>
+    <input type="text" id="brightnessInput"><br />
+    <button id='rainbow'>rainbow</button>
+    <button id='orange'>orange</button>
+    <div id='status'></div>
+  </body></html>
+  )");
+}
+
+void handleOrange() {
+  uint32 colorInt = 0xFF4301;
+  ws2812fx.setColor(colorInt);
+  ws2812fx.setMode(FX_MODE_STATIC);
+  ws2812fx.setBrightness(20);
+  ws2812fx.service();
+  String message = "setting color hex: " + intToStrHex(colorInt);
+  server.send(200, "text/html", message);
+}
+
+void handleColor() {
+  uint32 colorInt = 0; // default to black
+  // 0xFF4301
+
+  String message = "default color is" + colorInt;
+
+  String colorArg = server.arg("color");
+  if (colorArg != "") {
+    message = message + "<br> color arg is " + colorArg;
+    // colorInt = (int) strtol(colorArg, NULL, 16);
+    colorInt = strToHex(colorArg);
+  }
+
+  String colorHexStr = intToStrHex(colorInt);
+  message = message + "<br> setting color - int: " + String(colorInt) + " hex: " + colorHexStr;
+
+  ws2812fx.setColor(colorInt);
+  ws2812fx.setMode(FX_MODE_STATIC);
+  ws2812fx.setBrightness(20);
+  ws2812fx.service();
+
+  server.send(200, "text/html", message);
+}
+
+void handleBrightness() {
+  uint8 brightness = 0;
+  String brightnessArg = server.arg("brightness");
+  brightness = brightnessArg.toInt();
+  if (brightness < 0) {
+    brightness = 0;
+  }
+  if (brightness > 255) {
+    brightness = 255;
+  }
+
+  ws2812fx.setBrightness(brightness);
+  ws2812fx.service();
+
+  String message = "set brightness to " + String(brightness);
+  server.send(200, "text/html", message);
+}
+
+void handleRainbow() {
+  uint8 mode = FX_MODE_RAINBOW_CYCLE;
+
+  ws2812fx.setSpeed(DEFAULT_SPEED);
+  ws2812fx.setBrightness(5);
+  ws2812fx.setMode(mode);
+  ws2812fx.start();
+  ws2812fx.show();
+  ws2812fx.service();
+
+  String message = "mode set to " + String(mode);
+  // String message = "mode set to ";
+  server.send(200, "text/html", message);
+}
+
+// TODO: set fire mode
+// TODO: cycle between modes
+
+int strToHex(String str) {
+  const char * c = str.c_str();
+  return (int) strtol(c, 0, 16);
+}
+
+String intToStrHex(uint32 color) {
+  String colorStr = String(color, HEX);
+  colorStr.toUpperCase();
+  return colorStr;
 }
